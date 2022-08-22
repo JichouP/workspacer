32013c99f636d4061aacbbebac9953068a3fd2b7
JichouP <akkunn100@gmail.com>
2022-08-23 00:11:57 +0900

    :sparkles: add AppDir State

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,10 +3,30 @@
     windows_subsystem = "windows"
 )]
 
+#[macro_use]
+extern crate tauri;
+
 use anyhow::Result;
+use tauri::Manager;
+
+struct AppDir(String);
 
 fn main() -> Result<()> {
     tauri::Builder::default()
+        .setup(|app| {
+            let app_dir = app
+                .path_resolver()
+                .app_dir()
+                .unwrap()
+                .to_string_lossy()
+                .to_string();
+
+            let app_dir = AppDir(app_dir);
+
+            app.manage(app_dir);
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 
