a47b0f59f68f521510e445728cd9cabc48bb985a
JichouP <akkunn100@gmail.com>
2022-08-28 06:31:35 +0900

    :sparkles: apply Layout to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { FC } from 'react';
+import Layout from './components/Layout';
 
 const App: FC = () => {
-  return <div className="text-lg">Hello</div>;
+  return (
+    <Layout>
+      <div className="">Content</div>
+    </Layout>
+  );
 };
 
 export default App;
