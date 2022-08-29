9bae92694972feee43cb1f437c74d0328f2b6ce6
JichouP <akkunn100@gmail.com>
2022-08-29 13:43:17 +0900

    :sparkles: add Sidebar/Footer.tsx

---
+++ src/components/Layout/Sidebar/Footer.tsx
@@ -0,0 +1,13 @@
+import MadeWithLove from './Footer/MadeWithLove';
+import Version from './Footer/Version';
+
+const Footer = () => {
+  return (
+    <footer className="text-center">
+      <Version></Version>
+      <MadeWithLove></MadeWithLove>
+    </footer>
+  );
+};
+
+export default Footer;
