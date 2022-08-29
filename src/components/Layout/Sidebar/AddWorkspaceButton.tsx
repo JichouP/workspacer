de1fbd826f00d52a3b8a1b309ead9fbf967d64d1
JichouP <akkunn100@gmail.com>
2022-08-29 22:29:04 +0900

    :sparkles: add AddWorkspaceButton.tsx

--- src/components/Layout/Sidebar/AddWorkspaceButton.tsx
+++ src/components/Layout/Sidebar/AddWorkspaceButton.tsx
@@ -1,9 +1,13 @@
 import { FC } from 'react';
 import AddIcon from './AddIcon';
 
-const AddWorkspaceButton: FC = () => {
+type Props = {
+  onClick: () => void;
+};
+
+const AddWorkspaceButton: FC<Props> = () => {
   return (
-    <button className="flex w-full flex-row items-center justify-center text-center">
+    <button className="btn btn-ghost flex w-full flex-row items-center justify-center normal-case">
       <AddIcon></AddIcon>
       <span className="m-2 text-lg">Add Workspace</span>
     </button>
