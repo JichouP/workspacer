a3a1286c747c1fb0e02c93cfe95fe982bf7cc3b1
JichouP <akkunn100@gmail.com>
2022-09-10 21:33:53 +0900

    :sparkles: add Spinner.tsx

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="h-10 w-10 animate-spin rounded-full border-4 border-blue-500 border-t-transparent"></div>
+    </div>
+  );
+};
+
+export default Spinner;
