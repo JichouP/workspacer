e0acf0217850b402c5e3e9f899bc7be52cd66c1c
JichouP <akkunn100@gmail.com>
2022-09-10 11:15:29 +0900

    :bug: fix launch_code

--- src-tauri/src/cmd/launch_code.rs
+++ src-tauri/src/cmd/launch_code.rs
@@ -2,7 +2,8 @@ use std::process::Command;
 
 #[command]
 pub fn launch_code(path: String) -> Result<(), String> {
-    let child = Command::new("code").arg(path).spawn();
+    let child = Command::new("cmd").args(["/C", "code", &path]).spawn();
+    // let child = Command::new("code").arg(path).spawn();
 
     match child {
         Ok(_) => Ok(()),
