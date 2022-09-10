23918a6c0be57e9d48c0587a5cc1cd3328f824c8
JichouP <akkunn100@gmail.com>
2022-09-10 14:59:55 +0900

    :sparkles: add Header.tsx

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,7 +1,20 @@
+import currentWorkspaceState from '@/atoms/currentWorkspaceState';
 import { FC } from 'react';
+import { useRecoilState } from 'recoil';
+import RemoveWorkspaceButton from './Header/RemoveWorkspaceButton';
+import RemoveWorkspaceModal from './Header/RemoveWorkspaceModal';
 
 const Header: FC = () => {
-  return <div className="h-16 bg-base-300">Header</div>;
+  const [currentWorkspace] = useRecoilState(currentWorkspaceState);
+  return (
+    <div className="flex h-16 flex-row items-center bg-base-300 p-4">
+      <span className="flex-1">
+        <pre>{currentWorkspace}</pre>
+      </span>
+      {currentWorkspace && <RemoveWorkspaceButton></RemoveWorkspaceButton>}
+      <RemoveWorkspaceModal></RemoveWorkspaceModal>
+    </div>
+  );
 };
 
 export default Header;
