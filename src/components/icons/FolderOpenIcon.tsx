272c0bd2b95f928592cb9e936a5d1c127ac030e9
JichouP <akkunn100@gmail.com>
2022-09-10 20:51:27 +0900

    :sparkles: add FolderOpenIcon

---
+++ src/components/icons/FolderOpenIcon.tsx
@@ -0,0 +1,20 @@
+const FolderOpenIcon = () => {
+  return (
+    <svg
+      className="h-full w-full"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M5 19a2 2 0 01-2-2V7a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1M5 19h14a2 2 0 002-2v-5a2 2 0 00-2-2H9a2 2 0 00-2 2v5a2 2 0 01-2 2z"
+      />
+    </svg>
+  );
+};
+
+export default FolderOpenIcon;
