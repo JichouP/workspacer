f4defefa3213290cb1189daab36907b68043e8dc
JichouP <akkunn100@gmail.com>
2022-09-10 11:06:18 +0900

    :recycle: fix arg name

--- src-tauri/src/cmd/add_workspace.rs
+++ src-tauri/src/cmd/add_workspace.rs
@@ -2,6 +2,6 @@ use crate::infrastructure::state::ConfigState;
 use std::path::PathBuf;
 
 #[command]
-pub fn add_workspace(repository: ConfigState, workspace: PathBuf) -> Vec<PathBuf> {
-    crate::usecase::config::add_workspace(repository.inner(), workspace).unwrap()
+pub fn add_workspace(repository: ConfigState, path: PathBuf) -> Vec<PathBuf> {
+    crate::usecase::config::add_workspace(repository.inner(), path).unwrap()
 }
--- src-tauri/src/cmd/remove_workspace.rs
+++ src-tauri/src/cmd/remove_workspace.rs
@@ -2,6 +2,6 @@ use crate::infrastructure::state::ConfigState;
 use std::path::PathBuf;
 
 #[command]
-pub fn remove_workspace(repository: ConfigState, workspace: PathBuf) {
-    crate::usecase::config::remove_workspace(repository.inner(), workspace).unwrap();
+pub fn remove_workspace(repository: ConfigState, path: PathBuf) {
+    crate::usecase::config::remove_workspace(repository.inner(), path).unwrap();
 }
