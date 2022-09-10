c344ff7ab3956849b8f5cf3edd1066a978fe69aa
JichouP <akkunn100@gmail.com>
2022-09-10 18:06:11 +0900

    :sparkles: add Sidebar.tsx

--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -1,11 +1,39 @@
+import currentWorkspaceState from '@/atoms/currentWorkspaceState';
+import workspacesState from '@/atoms/workspacesState';
 import { FC } from 'react';
+import { useRecoilState } from 'recoil';
 import Logo from './Logo';
+import AddWorkspaceButton from './Sidebar/AddWorkspaceButton';
+import AddWorkspaceModal from './Sidebar/AddWorkspaceModal';
+import Footer from './Sidebar/Footer';
+import WorkspaceButton from './Sidebar/WorkspaceButton';
 
 const Sidebar: FC = () => {
+  const [workspaces] = useRecoilState(workspacesState);
+  const [, setCurrentWorkspace] = useRecoilState(currentWorkspaceState);
+
   return (
-    <div className="w-60 bg-base-200">
-      <Logo></Logo>
-      <div className="divider m-0 h-0"></div>
+    <div className="flex w-96 flex-col items-center justify-between bg-base-200">
+      <div className="flex w-full flex-col items-center justify-center">
+        <div className="h-16 text-gray-200">
+          <Logo></Logo>
+        </div>
+        <div className="divider m-0 h-0 w-full"></div>
+      </div>
+      <div className="w-full flex-1">
+        {workspaces.map((v) => (
+          <WorkspaceButton
+            key={v}
+            path={v}
+            onClick={() => setCurrentWorkspace(v)}
+          ></WorkspaceButton>
+        ))}
+        <AddWorkspaceButton onClick={() => null}></AddWorkspaceButton>
+        <AddWorkspaceModal></AddWorkspaceModal>
+      </div>
+      <div>
+        <Footer></Footer>
+      </div>
     </div>
   );
 };
