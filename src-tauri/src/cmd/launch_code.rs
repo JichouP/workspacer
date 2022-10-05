52e1d19726c892c8b3fab9198eb148659066c39b
JichouP <akkunn100@gmail.com>
2022-10-05 22:02:31 +0900

    :sparkles: support wsl

--- src-tauri/src/cmd/launch_code.rs
+++ src-tauri/src/cmd/launch_code.rs
@@ -2,15 +2,34 @@ use std::process::Command;
 
 #[command]
 pub fn launch_code(path: String) -> Result<(), String> {
-    let shell = match cfg!(windows) {
-        true => "cmd",
-        false => "sh",
-    };
-
-    Command::new(shell)
-        .args(["/C", "code", &path])
-        .spawn()
-        .map_err(|e| e.to_string())?;
+    if cfg!(windows) {
+        if path.starts_with(r"\\wsl.localhost\") {
+            let path = path.replace(r"\\wsl.localhost\", "");
+            let path = path.split(&['/', '\\'][..]).collect::<Vec<_>>();
+            let distribution = path[0];
+            let path = path[1..].join("/");
+            Command::new("cmd")
+                .args([
+                    "/c",
+                    "code",
+                    "--remote",
+                    &format!("wsl+{}", distribution),
+                    &format!("/{}", &path),
+                ])
+                .spawn()
+                .map_err(|e| e.to_string())?;
+        } else {
+            Command::new("cmd")
+                .args(["/c", "code", &path])
+                .spawn()
+                .map_err(|e| e.to_string())?;
+        }
+    } else {
+        Command::new("sh")
+            .args(["-c", "code", &path])
+            .spawn()
+            .map_err(|e| e.to_string())?;
+    }
 
     Ok(())
 }
